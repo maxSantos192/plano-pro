8c4be55216b029d218345abbd21924292c755a69
Maxson Santos <maxsonsantos88@gmail.com>
2025-06-11 19:04:57 -0300

    feat: update title project and globals styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Planos de aula",
+  title: "PlanoPro",
   description: "",
 };
 
@@ -22,4 +23,3 @@ export default function RootLayout({
     </html>
   );
 }
-
