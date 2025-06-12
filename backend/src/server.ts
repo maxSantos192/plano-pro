42d868f915c3490bfffb679cf7070afe1b195643
Maxson Santos <maxsonsantos88@gmail.com>
2025-06-12 18:24:47 -0300

    feat: add health check endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,15 @@ import { router } from "./routes";
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use("/", router);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timesTamp: new Date().toISOString(),
+    message: "PlanoPro API is running",
+  });
+});
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`Server is running on port ${process.env.PORT || 3333}`);
