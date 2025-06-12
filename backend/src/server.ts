7f4582f8308df0cac5ef4fe6a434980d780b13db
Maxson Santos <maxsonsantos88@gmail.com>
2025-06-12 11:25:38 -0300

    feat: add express server setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello wolrd");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3333}`);
+});
